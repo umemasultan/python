3e92b17d89e54ce73efa979c6f7f0c895e549c5d
umemasultan <umemasultan11@gmail.com>
2025-02-15 19:59:12 +0500

    traffic rules

---
+++ light.py
@@ -0,0 +1,15 @@
+
+# /// Traffic Lights///
+
+light=(input(" Traffic Lights Please follow thesered:"))
+if (light =="Red"):
+    print("Stop a red light today, a safe journey tomorrow! 🚦🔴")
+
+elif (light =="Yellow"):
+    print("A moment of patience, a lifetime of safety!🚦🟡")
+
+elif (light =="Green"):
+    print("Now is the time, move forward! 🟢➡️")
+
+else:
+    print("A few seconds of impatience can cost a lifetime. ⏱️💔")
