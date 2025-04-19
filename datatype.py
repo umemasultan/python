cc7bd5dde44e8535b0d6bb4f0d83a08ea7f5ce0a
umemasultan <umemasultan11@gmail.com>
2025-04-19 11:53:54 +0500

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,75 @@
+#  data type in python
+
+# a = 1
+# b = 1
+# print(a + b)
+# print(type(a))  # checking data type : int
+
+# c = "1"
+# d = "1"
+# print(c + d)
+# print(type(c))    # checking data type : str
+
+
+# basic data types in python :
+# # 1) Numeric
+# a1 = 1   # 1a. integer
+# a2 = 1.5 # 1b. float
+# print(type(a2))
+# a3 = complex (3,5) # 1c.complex
+# print(type(a3))
+
+
+# # 2) Sequence
+# b1 = "umema" #2a. string 
+# print(type(b1))
+# b2 =[1, 2 ,33,45,6,718, "umema" ]#2b . list 
+# print(type(b2))
+# b3 =[1, 2 ,33,45,6,718, "umema" ]
+# print(type(b3))
+
+
+# #3) Dictionoary
+# my_dict ={
+#     "name":"umema",
+#     "age" :19 ,
+#     "city":"karachi"
+# }
+# print (type(my_dict))
+
+#4)Sets
+
+# my_sets ={1 ,2 ,3 ,4,345 ,'umema'}
+# print (type(my_sets))
+
+# 5) Boolean
+
+# is_pass= True
+# is_pass = False
+# print (type(is_pass))
+
+#6) Binary 
+# bytes , bytearray, memoryview
+# byte = b"umema"
+# print (type(byte))
+
+# inverted commas diffrence
+
+print("Hello World")
+print('Hello World')
+print("Hello World that's great")
+print('''Hello World what's going on''')
+print("Hello.\n\"How are you\" and 'when yor exam' now whats happend")
+
+# many content print in one sentence
+name="umema"
+age =19
+city="karachi"
+print(f"my name is :{name}  from: {city}  & I'm:{age}" )
+
+
+
+
+
+
+
